0e3892101eb04dfad1d4e609ec016ba2a39d97d0
Cesar Martinez <cmartinezaraujo@ewu.edu>
2022-05-30 11:26:48 -0700

    Shapes on screen
    *Added Two.js library
    *Got initial shapes on screen

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //My scripts
-console.log("Hello World");
+
+// Get drawing for drawing
+const container = document.querySelector("section");
+
+// Set parameters for container
+let params = {
+  width: 1000,
+  height: 1000
+};
+
+// `new` instantiation Two.js
+const two = new Two(params);
+// append to `container`
+two.appendTo(container);
+
+let shape = two.makeRectangle(10, 10, 20, 20);
+shape.fill = "black";
+
+let shape2 = two.makeRectangle(30, 10, 20, 20);
+shape.fill = "black";
+
+
+two.play();
+
